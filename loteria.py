27a445fbc5335d9fd7eb4aff242a288c0dd9ebf6
Douglas Silveira Jr <douglas.silveira.junior@gmail.com>
2021-12-31 13:52:13 -0300

    Create loteria.py

---
+++ loteria.py
@@ -0,0 +1,16 @@
+import numpy as np
+import time
+def jogo():
+  rng = np.random.default_rng()
+  return sorted(rng.choice(range(0,61), size=10, replace=False))
+def espera():
+  return(np.random.randint(0, 10))
+def jogar(qtde):
+  for i in range(qtde):
+    delay = espera()
+    print('esperando', delay, 'segundos para o próximo jogo')
+    time.sleep(delay)
+    combinacao = jogo()
+    
+    print('Jogo', i+1, ':' , combinacao)
+jogar(25)
